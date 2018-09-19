2f27dc5ab53423a2d0e1699f85517bd870e3b4a0
Manuel Isaac Martinez Torres <manuel.martinez@kit.edu>
2018-09-19 05:41:05 +0200

    iup

--- src/common/menu.c
+++ src/common/menu.c
@@ -21,90 +21,152 @@ void printStrRAW(uint16_t baseAddress, uint8_t x, uint8_t y, const char *msg) {
 	}
 	TMS9918_memcpy(baseAddress + x+(y<<5),tiles,l);
 }
-
-
-T_f M0_menu() {	
-
-	uint8_t freeTiles[256];
-	uint8_t slim[32], bold[32];
-
-	TMS9918_setFlags(TMS9918_M2 | TMS9918_BLANK | TMS9918_GINT | TMS9918_MEM416K | TMS9918_SI | TMS9918_MAG);	
-
-
-	if (0) {
-		uint8_t c;
-		for (c = 0; c<255; c++) freeTiles[c]=1;
+/*
 		for (c = 32; c<128; c++) {
 			uint8_t l, v;
 			U8x8 shape, color;
 			for (l=0; l<8; l++) color[l] = BBlack+FGray;
 			getMSXROMTile(shape, c);
-
 			v = addASCIITile(freeTiles, c, shape, color);
-			//printf("%d\n", v);
-			if (c==32)
-				TMS9918_memset(ADDRESS_PN0, v, sizeof(T_PN));
 		}
-		printStr(ADDRESS_PN0, 5,17,"Choose Wisely:");
+
+ */
+
+typedef struct {uint8_t slim[32], bold[32];} TMsg; 
+
+static void colorMsg(const TMsg *msg) {
+	
+	uint8_t i;
+	U8x8 color;
+	for (i=0; i<8; i++) color[i] = BBlack + FLightRed;
+	for (i=0; msg->slim[i]; i++) 
+		TMS9918_memcpy(ADDRESS_CT + (((uint16_t)msg->slim[i])<<3), color, 8);
+
+	for (i=0; i<8; i++) color[i] = BBlack + FGray;
+	for (i=0; msg->bold[i]; i++) 
+		TMS9918_memcpy(ADDRESS_CT + (((uint16_t)msg->bold[i])<<3), color, 8);
+}
+
+T_f M0_menu() {	
+
+	uint8_t freeTiles[256];
+	uint8_t space;
+	TMsg MchooseWisely;
+	TMsg MmarieAntoniette;
+	TMsg MlouisCapet;
+	TMsg MraceMode;
+	
+	TMsg MleftTriangle, MrightTriangle;
+	
+
+	{
+		uint8_t c;
+		for (c = 0; c<255; c++) freeTiles[c]=1;
+	}
+	{
+		uint8_t l, v;
+		U8x8 shape, color;
+		for (l=0; l<8; l++) {
+			shape[l] = 0;
+			color[l] = BBlack+FGray;
+		}
+		space = addASCIITile(freeTiles, ' ', shape, color);
+		TMS9918_memset(ADDRESS_PN0, space, sizeof(T_PN));
 	}
 	
-	{
-		initRenderedText(slim, bold, freeTiles, "Estic Flipant! Ha funcionat :)!", font_zelda, 1);
-		
-		{
-			uint8_t i;
-			U8x8 color;
-			for (i=0; i<8; i++) color[i] = BBlack + FLightRed;
-			for (i=0; slim[i]; i++) 
-				TMS9918_memcpy(ADDRESS_CT + (((uint16_t)slim[i])<<3), color, 8);
 
-			for (i=0; i<8; i++) color[i] = BBlack + FGray;
-			for (i=0; bold[i]; i++) 
-				TMS9918_memcpy(ADDRESS_CT + (((uint16_t)bold[i])<<3), color, 8);
-		}
-	}
-	if (0) {
-		printStr(ADDRESS_PN0, 5,19,"DONE!");
-	}
+	initRenderedText(MchooseWisely.slim, MchooseWisely.bold, freeTiles, "Capet's Race!", font_zelda, 1);
+	initRenderedText(MmarieAntoniette.slim, MmarieAntoniette.bold, freeTiles, "Marie Antoniette", font_zelda, 1);
+	initRenderedText(MlouisCapet.slim, MlouisCapet.bold, freeTiles, "Louis Capet", font_zelda, 3);
+	initRenderedText(MraceMode.slim, MraceMode.bold, freeTiles, "vs. Mode", font_zelda, 3);
+	initRenderedText(MleftTriangle.slim, MleftTriangle.bold, freeTiles, "\x11", font_zelda, 3);
+	initRenderedText(MrightTriangle.slim, MrightTriangle.bold, freeTiles, "\x10", font_zelda, 3);
+	colorMsg (&MchooseWisely);
+	colorMsg (&MmarieAntoniette);
+	colorMsg (&MlouisCapet);
+	colorMsg (&MraceMode);
+	colorMsg (&MrightTriangle);
+	colorMsg (&MleftTriangle);
 
+	TMS9918_setFlags(TMS9918_M2 | TMS9918_BLANK | TMS9918_GINT | TMS9918_MEM416K | TMS9918_SI | TMS9918_MAG);	
 
 	{
+		int8_t selection = 0;
 		T_SA SA;
 		uint8_t f = 0;	
 		while (TRUE) {
 			
-			SA[0].x = (256/3)-16;
-			SA[0].y = 80-14;
+			SA[0].x = (256/4)-16;
+			SA[0].y = 50-14;
 			SA[0].pattern = SPRITE_HAIR_MA;
 			SA[0].color = BWhite;
 
-			SA[1].x = (256/3)-16;
-			SA[1].y = 80;
+			SA[1].x = (256/4)-16;
+			SA[1].y = 50;
 			SA[1].pattern = SPRITE_HEAD_LEFT +((f>>2)<<2);
 			SA[1].color = BLightRed;
 			if (f&1) SA[1].color = BWhite;
+			if (selection==1) SA[1].pattern = SPRITE_HEAD_LEFT;
+			
 
-			SA[2].x = (2*256/3)-16;
-			SA[2].y = 80-2;
+			SA[2].x = (3*256/4)-16;
+			SA[2].y = 50-2;
 			SA[2].pattern = SPRITE_HAIR_LC;
 			SA[2].color = BWhite;
 
-			SA[3].x = (2*256/3)-16;
-			SA[3].y = 80;
+			SA[3].x = (3*256/4)-16;
+			SA[3].y = 50;
 			SA[3].pattern = SPRITE_HEAD_RIGHT + ((15-(f>>2))<<2);
 			SA[3].color = BLightYellow;
 			if (f&1) SA[3].color = BWhite;
-
+			if (selection==0) SA[3].pattern = SPRITE_HEAD_RIGHT;
 
 			SA[4].y = 208;
 			
 			f++;
 			f= f&(0xFF>>2);
+			
+			//TMS9918_memset(ADDRESS_PN0, space, sizeof(T_PN));
 
-			if (f&1)
-				printStrRAW(ADDRESS_PN0, 5,18,slim);
-			else
-				printStrRAW(ADDRESS_PN0, 5,18,bold);
+			if (f&1){
+				printStrRAW(ADDRESS_PN0, 12,14,MchooseWisely.slim);
+				printStrRAW(ADDRESS_PN0, 11,16,MmarieAntoniette.slim);
+				printStrRAW(ADDRESS_PN0, 12,17,MlouisCapet.slim);
+				printStrRAW(ADDRESS_PN0, 13,18,MraceMode.slim);
+				
+				printStrRAW(ADDRESS_PN0,  9,16+selection,MrightTriangle.slim);
+				printStrRAW(ADDRESS_PN0, 21,16+selection,MleftTriangle.slim);
+
+			} else {
+				printStrRAW(ADDRESS_PN0, 12,14,MchooseWisely.bold);
+				printStrRAW(ADDRESS_PN0, 11,16,MmarieAntoniette.bold);
+				printStrRAW(ADDRESS_PN0, 12,17,MlouisCapet.bold);
+				printStrRAW(ADDRESS_PN0, 13,18,MraceMode.bold);
+				
+				printStrRAW(ADDRESS_PN0,  9,16+selection,MrightTriangle.bold);
+				printStrRAW(ADDRESS_PN0, 21,16+selection,MleftTriangle.bold);
+			}
+
+			{
+				uint8_t keys = keyboard_read();
+				
+				if (keys & KEYBOARD_UP) {
+					if (!selection) 
+						selection = 2;
+					else
+						selection--;
+				}
+
+				if (keys & KEYBOARD_DOWN) {
+					if (selection==2) 
+						selection = 0;
+					else
+						selection++;
+				}
+
+				if (keys & KEYBOARD_SPACE) {
+				}
+			}
 			
 			TMS9918_memcpy(ADDRESS_SA0,(uint8_t *)SA,32);
 			TMS9918_waitFrame();
--- src/common/nicefonts.c
+++ src/common/nicefonts.c
@@ -2,27 +2,26 @@
 
 #define INIT(a,v) { register uint8_t value = v; register uint8_t sz = sizeof(a); register uint8_t *p = (uint8_t *)a; while (sz--) *p++=value; }
 
-#define uintXX_t uint32_t
+#define uintXX_t uint16_t
 #define XX (8*sizeof(uintXX_t))
 
-static void pasteCharacter( uintXX_t workAreaSlim[8], uintXX_t workAreaBold[8], const TFont font, uint8_t c, uint8_t maxDisplacement_) {
+static void pasteCharacter(uintXX_t workAreaSlim[8], uintXX_t workAreaBold[8], const TFont font, uint8_t c, uint8_t maxDisplacement_) {
 
 	uint8_t i;
 	register uint8_t maxDisplacement = maxDisplacement_;
-	register uint8_t *f = font[c];
+	register const uint8_t *f = font[c];
 	for (i=0; i<8; i++) {
 		
-		uintXX_t fi = f[i];
-		
-		workAreaSlim[i] |= fi << maxDisplacement;
+		workAreaSlim[i] |= ((uintXX_t)f[i]) << maxDisplacement;
 		
 		workAreaBold[i] |= 
-			(fi << (maxDisplacement+1)) | 
-			(fi << (maxDisplacement+0)) | 
-			(fi << (maxDisplacement-1));
+			(((uintXX_t)f[i]) << (maxDisplacement+1)) | 
+			(((uintXX_t)f[i]) << (maxDisplacement+0)) | 
+			(((uintXX_t)f[i]) << (maxDisplacement-1));
 	}
 
 	for (i=0; i<7; i++) {
+		
 		workAreaBold[i+1] |= ((uintXX_t)f[i]) << (maxDisplacement+1);
 		workAreaBold[i+1] |= ((uintXX_t)f[i]) << (maxDisplacement+0);
 		workAreaBold[i+1] |= ((uintXX_t)f[i]) << (maxDisplacement-1);
@@ -33,7 +32,7 @@ static void pasteCharacter( uintXX_t workAreaSlim[8], uintXX_t workAreaBold[8], 
 	}
 }
 
-static uint8_t writeShape( uintXX_t workArea[8], uint8_t freeTiles[256] ) {
+static uint8_t writeShape(uintXX_t workArea[8], uint8_t freeTiles[256]) {
 	
 	
 	U8x8 shape;
@@ -58,7 +57,7 @@ static uint8_t writeShape( uintXX_t workArea[8], uint8_t freeTiles[256] ) {
 
 void initRenderedText(uint8_t *slim, uint8_t *bold, uint8_t freeTiles[256], const uint8_t *str, const TFont font, uint8_t padding) {
 	
-	uint8_t maxDisplacement = XX-8-padding;
+	int8_t maxDisplacement = XX-8-padding;
 	uintXX_t workAreaSlim[8];
 	uintXX_t workAreaBold[8];
 	INIT(workAreaSlim,0);
@@ -69,38 +68,49 @@ void initRenderedText(uint8_t *slim, uint8_t *bold, uint8_t freeTiles[256], cons
 		uint8_t c = *str++;
 		pasteCharacter(workAreaSlim,workAreaBold,font,c,maxDisplacement);
 		maxDisplacement -= 8;
+		printf("A: %d\n",maxDisplacement);
 	}
 	while (*str) {
-		uint8_t c = *str++;
-		uint8_t mx = min(XX-8-padding + 2, maxDisplacement+6);
+		uint8_t c = *str;
+		int8_t mx = min(XX-8, maxDisplacement+6);
+		printf("MX A: %d %d\n",maxDisplacement, mx);
 		
 		while (maxDisplacement < mx) {
 			uint8_t i;
-			for (i=0; i<8; i++) 
-				if ((workAreaSlim[i] & (((uintXX_t)font[c][i]) << maxDisplacement)) != 0) 
-					goto out;
+			if (maxDisplacement>=0)
+				for (i=0; i<8; i++) 
+					if ((workAreaSlim[i] & (((uintXX_t)font[c][i]) << maxDisplacement)) != 0) 
+						goto out;
+			if (maxDisplacement< 0)
+				for (i=0; i<8; i++) 
+					if ((workAreaSlim[i] & (((uintXX_t)font[c][i]) >> -maxDisplacement)) != 0) 
+						goto out;
 			maxDisplacement++;
 		}
 		out:
-		maxDisplacement -= 2;
-		pasteCharacter(workAreaSlim,workAreaBold,font,c,maxDisplacement);
+
+		printf("MX B: %d %d\n\n",maxDisplacement, mx);
 		
-		maxDisplacement -= 8;
-		
-		if (maxDisplacement<10) {
+		if (maxDisplacement<=2) {
 			
 			*slim++ = writeShape(workAreaSlim, freeTiles);
 			*bold++ = writeShape(workAreaBold, freeTiles);
 			maxDisplacement += 8;
+			continue;
 		}
+		
+		maxDisplacement -= 2;
+		pasteCharacter(workAreaSlim,workAreaBold,font,c,maxDisplacement);
+		maxDisplacement -= 8;
+		str++;
 	}
-	while (maxDisplacement<(XX-8+padding)) {
+	while (TRUE) {
+		
+		uint8_t vs, vb;
 
-		*slim++ = writeShape(workAreaSlim, freeTiles);
-		*bold++ = writeShape(workAreaBold, freeTiles);
-		maxDisplacement += 8;
+		*slim++ = vs = writeShape(workAreaSlim, freeTiles);
+		*bold++ = vb = writeShape(workAreaBold, freeTiles);
+		if (!(vs || vb)) break;
+		
 	}
-	
-	*slim++ = 0;
-	*bold++ = 0;
 }
