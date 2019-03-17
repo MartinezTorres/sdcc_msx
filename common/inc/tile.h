441dfd89e0db63453d4a54741af2eee34b51b35b
Manuel Isaac Martinez Torres <manuel.martinez@kit.edu>
2019-03-17 11:14:52 +0100

    asdf

--- common/inc/tile.h
+++ common/inc/tile.h
@@ -3,11 +3,6 @@
 #include <tms9918.h>
 
 ////////////////////////////////////////////////////////////////////////
-// Font API
-//
-typedef U8x8 TFont[128];
-
-////////////////////////////////////////////////////////////////////////
 // Tile Manager API
 //
 // Used to reserve and free tiles.
@@ -19,23 +14,6 @@ void    M2_allocTile   (EM2_PGpage pgpage, TileIdx tileIdx);
 void    M2_freeTile    (EM2_PGpage pgpage, TileIdx tileIdx);
 void    M2_freeAllTiles(EM2_PGpage pgpage);
 
-////////////////////////////////////////////////////////////////////////
-// Monospace Font API
-//
-// Text API where each character belongs to a Tile.
-//
-
-typedef struct { uint8_t conv[256]; }
-T_M2_MS_Status;
-
-void M2_MS_clearpage();
-
-void M2_MS_loadChar(EM2_PGpage pgpage, const uint8_t idx, const U8x8 shape, const U8x8 color);
-void M2_MS_freeChar(EM2_PGpage pgpage, const uint8_t idx);
-
-void M2_MS_setPos  (EM2_Buffer buffer, const uint8_t x, const uint8_t y);
-void M2_MS_putchar (EM2_Buffer buffer, const uint8_t idx);
-void M2_MS_puts    (EM2_Buffer buffer, const char *msg);
 
 ////////////////////////////////////////////////////////////////////////
 // Proportional Font API
